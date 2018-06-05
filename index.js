667bdde56b61ba31db1f16ced28e3d9217660c25
Christoph Kieslich <christoph.kieslich@reaktor.com>
2018-06-05 15:44:03 +0200

    Refactor and use os module to derive hostname

--- index.js
+++ index.js
@@ -2,17 +2,13 @@
 
 var express = require('express')
 var app = express()
+const os = require('os')
+const hostname = os.hostname()
 
 app.get('/', function (req, res) {
-    const
-      spawn = require( 'child_process' ).spawn,
-      hostname = spawn( 'hostname' )
-    hostname.stdout.on('data', data =>{
-      res.send(`Hello from ${data}!`)
-    })
-
+  res.send(`Hello from ${hostname}!`)
 })
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!')
 })
